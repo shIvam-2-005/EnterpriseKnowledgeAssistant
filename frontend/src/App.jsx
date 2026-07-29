08681ce7d58a71ac3a3e340ef9dbd9d18170f4ae
shIvam-2-005 <shivamsharma37000@gmail.com>
2026-07-29 19:51:34 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Enterprise Knowledge Assistant 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
